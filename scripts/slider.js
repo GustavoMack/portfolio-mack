c6f7d8881f41bb9c4033b06d3fc02718b8b7f826
Gustavo Mack <160602347+GustavoMack@users.noreply.github.com>
2024-11-29 11:23:30 -0300

    Update slider.js

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,8 +1,8 @@
-let imagens = document.querySelectorAll(".imgs-carrosel");
-let imgPrincipal = document.querySelector("#img-principal");
+let imagens = document.querySelectorAll(".imgs-carrosel"); //variavel que seleciona tudo que tem a classe .imgs-carrosel
+let imgPrincipal = document.querySelector("#img-principal"); //variavel que seleciona o container que tem o id #img-principal
 
 imagens.forEach((imagem) => {
-  imagem.addEventListener("click", (event) => {
-    imgPrincipal.src = event.target.src; // Use o nome correto da variável
+  imagem.addEventListener("click", (event) => { // quando clicar
+    imgPrincipal.src = event.target.src; // ele pega o valor do id da variavel e troca pelo o que foi clicado
   });
 });
